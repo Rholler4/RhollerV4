1427cb2a0a8b1963bfc31637612c96e15528bbfb
Rholler4 <pi.rholleraws@gmail.com>
2024-03-06 10:44:15 -0800

    Fixed some of def update_graph_live

--- Rholler/Dash_Graphing/Plotly_Dash_Socket_Test_Client2.py
+++ Rholler/Dash_Graphing/Plotly_Dash_Socket_Test_Client2.py
@@ -47,22 +47,24 @@ y_values = [15]  # Start with a static Y value for diagnostics
 
 @app.callback(Output('real-time-graph', 'figure'),
               [Input('update-interval', 'n_intervals')])
-def update_graph_live():
+def update_graph_live(n):
     global x_values, y_values  # Use global variables to accumulate data
 
     print("Callback triggered")  # Confirm the callback is being triggered
-    print("Queue size before entering loop:", data_queue.qsize())  # Check the size of the queue
+    print("Queue size before entering loop:", data_queue.qsize())  # Check the size of the queue fixme: ALWAYS ZERO
 
-    while data_queue.empty():
+    # Change this line from 'while data_queue.empty():' to the following
+    while not data_queue.empty():  # This ensures the loop runs when the queue has data  fixme: NEVER ENTERS LOOP
         print("Entering the loop")  # This should print if the loop is entered
         try:
-            data = data_queue.get()
+            data = data_queue.get_nowait()  # Use get_nowait() to avoid blocking
             print("Plotting data:", data)  # This will print the data being processed
             print("Data type of 'x':", type(data['x']), "Data type of 'y':", type(data['y']))
             x_values.append(data['x'])
             y_values.append(data['y'])
         except Exception as e:
             print(f"Error processing data from queue: {e}")
+            break  # Exiting the loop if there's an error
 
     # Create a new trace with updated data
     trace = go.Scatter(x=x_values, y=y_values, mode='lines+markers', name='Random Data')
